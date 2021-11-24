5e4f98d8d13d172e069994489ada956203889ad0
PrimaIqbal <72261479+PrimaIqbal@users.noreply.github.com>
2021-11-24 14:56:57 +0700

    Add files via upload

---
+++ Tugas PBO_K3520062_Prima Iqbal Nurfaizi Nugroh/Pegawai.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author PRIMA IQBAL
+ */
+public class Pegawai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        public class ProjectSewa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+      rental.pilihan();    
+       System.out.print("Transaksi lagi Ya=1 Tidak=2 : ");
+        int hari=input.nextInt();
+        if(hari==1){
+            //  pemangilan kelas rental dan methode pilihan 
+            //nama class.nama methode
+            rental.pilihan();       
+        }    
+        
+      }  
+    }
+    }
+    
+}
